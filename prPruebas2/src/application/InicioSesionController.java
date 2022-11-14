0d21c58428453a6ee112d36c242a515038dc8051
Marcos-Garcia-Medina <74043202+Marcos-Garcia-Medina@users.noreply.github.com>
2022-11-14 14:05:54 +0100

    -Proyecto Creado

---
+++ prPruebas2/src/application/InicioSesionController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class InicioSesionController {
+	@FXML
+	private TextField txtNombre;
+
+	private PasswordField txtContrasena;
+	
+	
+}
