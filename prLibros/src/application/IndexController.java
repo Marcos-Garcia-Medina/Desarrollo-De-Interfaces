09f098bf3226bd9d6619d2cd9598e34731652813
Marcos-Garcia-Medina <74043202+Marcos-Garcia-Medina@users.noreply.github.com>
2022-11-28 14:26:15 +0100

    Funcion borrar´

--- prLibros/src/application/IndexController.java
+++ prLibros/src/application/IndexController.java
@@ -4,11 +4,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class IndexController {
@@ -33,6 +35,8 @@ public class IndexController {
 	private TableColumn <Juego,String> columnPegi;
 	@FXML
 	private Button btnAnadir;
+	@FXML
+	private Button btnBorrar;
 	
 	public ObservableList<Juego> listaVideojuegos =
 			FXCollections.observableArrayList(new Juego("Pepito Grillo",1,4,"PS4"));
@@ -60,14 +64,28 @@ public class IndexController {
 	
 	@FXML
 	public void anadirJuego(ActionEvent event) {
-		if(isNumeric(txtPrecio.getText())) {
-			Juego nuevoJuego = new Juego(
-				txtNombre.getText(),
-				Integer.parseInt(txtPrecio.getText())
-				,Integer.parseInt(choicePEGI.getValue().toString())
-				,choiceConsola.getValue().toString());
-			
-			listaVideojuegos.add(nuevoJuego);
+		if(!txtNombre.getText().isEmpty() || !txtPrecio.getText().isEmpty() || !choiceConsola.getSelectionModel().isEmpty() || !choicePEGI.getSelectionModel().isEmpty()) {
+			if(isNumeric(txtPrecio.getText())) {
+				Juego nuevoJuego = new Juego(
+					txtNombre.getText(),
+					Integer.parseInt(txtPrecio.getText())
+					,Integer.parseInt(choicePEGI.getValue().toString())
+					,choiceConsola.getValue().toString());
+				
+				listaVideojuegos.add(nuevoJuego);
+			}else {
+				Alert alerta = new Alert(AlertType.ERROR);
+				alerta.setTitle("Error al insertar");
+				alerta.setHeaderText("No se ha introducido un numero en las paginas.");
+				alerta.setContentText("Por favor, introduzca un numero en las paginas.");
+				alerta.showAndWait();
+			}
+		}else {
+			Alert alerta = new Alert(AlertType.WARNING);
+			alerta.setTitle("Error al insertar");
+			alerta.setHeaderText("Algun/os campo/s esta incompleto.");
+			alerta.setContentText("Por favor, revise el estado de los campos.");
+			alerta.showAndWait();
 		}
 		
 		txtNombre.clear();
@@ -78,7 +96,9 @@ public class IndexController {
 	
 	@FXML
 	public void borrarJuego(ActionEvent event) {
+		int indiceSeleccionado = tableVideojuegos.getSelectionModel().getSelectedIndex();
 		
+		tableVideojuegos.getItems().remove(indiceSeleccionado);
 	}
 	
 	public boolean isNumeric (String s) {
